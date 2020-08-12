8fea3ce515b6ce32ae0576024a450586492051a3
Alessandro Muraro <akmuraro@gmail.com>
2020-08-13 00:17:16 +0200

    adding feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPlugin(pluginRss);
 
   return {
     passthroughFileCopy: true,
